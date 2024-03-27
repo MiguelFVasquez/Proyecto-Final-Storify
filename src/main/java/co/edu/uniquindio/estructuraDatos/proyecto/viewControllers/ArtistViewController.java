bdcfb97c760309564c526b7def4a47bc18f47fc5
MiguelFVasquez <florezvasquezjuanmiguel@gmail.com>
2024-03-26 23:28:29 -0500

    Inicio de las interfaces (No estan para nada bien, Orozco plis

---
+++ src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/viewControllers/ArtistViewController.java
@@ -0,0 +1,4 @@
+package co.edu.uniquindio.estructuraDatos.proyecto.viewControllers;
+
+public class ArtistViewController {
+}
--- src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/viewControllers/SongViewController.java
+++ src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/viewControllers/SongViewController.java
@@ -1,4 +1,4 @@
 package co.edu.uniquindio.estructuraDatos.proyecto.viewControllers;
 
-public class InitViewController {
+public class SongViewController {
 }
