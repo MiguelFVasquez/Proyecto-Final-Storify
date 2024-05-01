a8715da923bf653ce3a569f9a3304891f44d0c2e
MiguelFVasquez <florezvasquezjuanmiguel@gmail.com>
2024-04-30 21:51:50 -0500

    Hilos de busqueda

--- src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/threads/ThreadO.java
+++ src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/threads/ThreadO.java
@@ -1,4 +1,25 @@
 package co.edu.uniquindio.estructuraDatos.proyecto.threads;
 
-public class ThreadO {
+import co.edu.uniquindio.estructuraDatos.proyecto.model.Enum.Gender;
+import co.edu.uniquindio.estructuraDatos.proyecto.model.Storify;
+
+public class ThreadO extends Thread {
+    private Storify storify;
+    private String artistName;
+    private String songName;
+    private String year;
+    private Gender gender;
+
+    public ThreadO(Storify storify, String artistName, String songName, Gender gender, String year) {
+        this.storify= storify;
+        this.artistName= artistName;
+        this.songName= songName;
+        this.year= year;
+        this.gender= gender;
+    }
+
+    @Override
+    public void run() {
+        storify.searchO(artistName,songName,gender,year);
+    }
 }
--- src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/threads/ThreadY.java
+++ src/main/java/co/edu/uniquindio/estructuraDatos/proyecto/threads/ThreadY.java
@@ -1,4 +1,25 @@
 package co.edu.uniquindio.estructuraDatos.proyecto.threads;
 
-public class ThreadY {
+import co.edu.uniquindio.estructuraDatos.proyecto.model.Enum.Gender;
+import co.edu.uniquindio.estructuraDatos.proyecto.model.Storify;
+
+public class ThreadY extends Thread{
+    private Storify storify;
+    private String artistName;
+    private String songName;
+    private String year;
+    private Gender gender;
+
+    public ThreadY(Storify storify, String artistName, String songName, String year, Gender gender) {
+        this.storify = storify;
+        this.artistName = artistName;
+        this.songName = songName;
+        this.year = year;
+        this.gender = gender;
+    }
+
+    @Override
+    public void run() {
+        storify.searchY(artistName,songName,gender,year);
+    }
 }
